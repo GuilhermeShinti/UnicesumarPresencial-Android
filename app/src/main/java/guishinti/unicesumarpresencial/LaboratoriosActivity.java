4c826ac078a69f59ae5b958bf348ea42c630aa05
GuilhermeShinti <guilhermeshinti@gmail.comgit>
2016-05-04 17:00:01 -0300

    Removido onPreExecute que não estava sendo utilizado, reaproveitando código loading e error, corrigindo pequeno bug que duplicava conteúdo da listView(lv_labs)

--- app/src/main/java/guishinti/unicesumarpresencial/LaboratoriosActivity.java
+++ app/src/main/java/guishinti/unicesumarpresencial/LaboratoriosActivity.java
@@ -5,22 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
 
-import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
-
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.util.HashMap;
 
 public class LaboratoriosActivity extends AppCompatActivity {
 
@@ -45,10 +36,7 @@ public class LaboratoriosActivity extends AppCompatActivity {
         bt_refresh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                lv_labs.setVisibility(View.INVISIBLE);
-                rl_telaErro.setVisibility(View.INVISIBLE);
-                rl_telaLoading.setVisibility(View.VISIBLE);
-                new AsyncDataClass().execute();
+                loading();
             }
         });
 
@@ -63,25 +51,32 @@ public class LaboratoriosActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 if (sp_status == true) {
                     sp_status = false;
-                    Lab.array.clear();
                     turnoParam = Turno.getParamTurnos(sp_turno.getSelectedItemPosition());
                     loading();
                 }
             }
-
             @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
+            public void onNothingSelected(AdapterView<?> parent) {}
         });
         loading();
     }
 
     public void loading(){
+        if (Conexao.verificaConexao(this)){
+            Lab.array.clear();
+            lv_labs.setVisibility(View.INVISIBLE);
+            rl_telaErro.setVisibility(View.INVISIBLE);
+            rl_telaLoading.setVisibility(View.VISIBLE);
+            new AsyncDataClass().execute();
+        }else{
+            error();
+        }
+    }
+
+    public void error(){
         lv_labs.setVisibility(View.INVISIBLE);
-        rl_telaErro.setVisibility(View.INVISIBLE);
-        rl_telaLoading.setVisibility(View.VISIBLE);
-        new AsyncDataClass().execute();
+        rl_telaLoading.setVisibility(View.INVISIBLE);
+        rl_telaErro.setVisibility(View.VISIBLE);
     }
 
     private class AsyncDataClass extends AsyncTask<String, Void, String> {
@@ -94,17 +89,11 @@ public class LaboratoriosActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(String... params) {
 
-//            html = new Pagina(HttpConnections.get(host+labs+Turno.getDiaAtual()+turnoParam));
             System.out.println(host+labs+Turno.getDiaAtual()+turnoParam);
             HttpGet url= new HttpGet(host+labs+Turno.getDiaAtual()+turnoParam);
 
             html = new Pagina(con.get(url));
             return html.toString();
-        }
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
         }
 
         @Override
@@ -114,9 +103,7 @@ public class LaboratoriosActivity extends AppCompatActivity {
             if(result.equals("") || result == null){
                 sp_status = true;
                 System.out.println("Server connection failed");
-                lv_labs.setVisibility(View.INVISIBLE);
-                rl_telaLoading.setVisibility(View.INVISIBLE);
-                rl_telaErro.setVisibility(View.VISIBLE);
+                error();
             }else{
                 sp_status = true;
                 PaginaLab.getPaginaLabs();
--- app/src/main/java/guishinti/unicesumarpresencial/MainActivity.java
+++ app/src/main/java/guishinti/unicesumarpresencial/MainActivity.java
@@ -1,5 +1,6 @@
 package guishinti.unicesumarpresencial;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent it = new Intent(MainActivity.this, LaboratoriosActivity.class);
+        startActivity(it);
+        finish();
     }
 }
